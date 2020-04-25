c52de29a7ba1bfeb867c29292b47c861784b15dd
D3JF <62910865+D3JF@users.noreply.github.com>
2020-04-25 17:30:30 +0200

    Fixed version

--- src/main/java/net/oldhaven/framework/Install.java
+++ src/main/java/net/oldhaven/framework/Install.java
@@ -263,7 +263,7 @@ public class Install {
     public static void checkLauncherUpdate() {
 
         // This is a very dirty way of setting a version, I know. I'll change it later. Maybe.
-        String currentVersion = "0.1.2";
+        String currentVersion = "0.1.3";
 
         try {
             URL oracle = new URL("https://api.github.com/repos/OldHaven-Network/BB-Launcher/releases");
